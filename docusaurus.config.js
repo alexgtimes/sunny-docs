ad22bb9fee37f9ab3a533df806b41439146695cf
alexgtimes <143785637+alexgtimes@users.noreply.github.com>
2024-05-14 13:27:34 +0930

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,7 +69,7 @@ const config = {
         title: 'sunny group',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/sunny.svg',
         },
         items: [
           {
